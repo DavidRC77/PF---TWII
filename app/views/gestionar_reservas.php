08ace07366e05cbd45dfbf254d2227ce2a8708f5
DavidRC <robles.carvajal77@gmail.com>
2026-03-09 01:21:46 -0400

    Estable 4

--- app/views/gestionar_reservas.php
+++ app/views/gestionar_reservas.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reservas de Hoy</title>
+    <title>Reservas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="public/assets/css/gestionar_reservas.css">
 </head>
